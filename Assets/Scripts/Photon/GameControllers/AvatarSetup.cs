7c06693d8e5b95edb6d2c4644bbd9bc226674502
Nemanja Djokic <borcanac.c1@gmail.com>
2020-11-28 00:02:08 +0100

    Avatar Setup And Character Picker

---
+++ Assets/Scripts/Photon/GameControllers/AvatarSetup.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myCharacter;
+    public int characterValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.PI.mySelectedCharacter);
+
+        }
+    }
+
+   
+    [PunRPC]
+    void RPC_AddCharacter(int whichCharacter)
+    {
+        characterValue = whichCharacter;
+        myCharacter = Instantiate(PlayerInfo.PI.allCharachters[whichCharacter], transform.position, transform.rotation, transform);
+    }
+}
