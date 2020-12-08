d832a95dc6349c2c5a738b4a284e46cd8c80be53
Miroslav Ilic <southgamesstudio@gmail.com>
2020-12-08 11:51:21 +0100

    Revert "v1.5"
    This reverts commit 18bcc23f2deb418f6a2a66132144fd94dfa11aac.

--- Assets/Scripts/Gallery/DragAndDropItem.cs
+++ Assets/Scripts/Gallery/DragAndDropItem.cs
@@ -107,12 +107,8 @@ public class DragAndDropItem : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void setPlayerName(string name)
     {
-        if (draggedItem != null)
-        {
-            int itemToSend = draggedItem.GetComponent<PhotonView>().ViewID;
-            GetComponent<PhotonView>().RPC("PlaceNameSync", RpcTarget.AllBuffered, itemToSend, PhotonNetwork.LocalPlayer.NickName);
-
-        }
+        int itemToSend = draggedItem.GetComponent<PhotonView>().ViewID;
+        GetComponent<PhotonView>().RPC("PlaceNameSync", RpcTarget.AllBuffered, itemToSend, PhotonNetwork.LocalPlayer.NickName);
     }
 
    
